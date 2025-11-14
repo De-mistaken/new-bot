f6f5690599380fff28f4be9625c749f9ae1b91ef
sir-bravin111 <mrbravin69@gmail.com>
2025-11-14 19:03:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
-const adams = require("./config");
+const bravo = require("./config");
 
 async function fetchBODYUrl() {
   try {
-    const response = await axios.get(adams.BWM_XMD);
+    const response = await axios.get(bravo.jeepers_creeper);
     const $ = cheerio.load(response.data);
 
     const targetElement = $('a:contains("BODY")');
