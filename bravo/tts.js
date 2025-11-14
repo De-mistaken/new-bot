80cfda0714532a28ee28629a2eaaae047a15ecbb
sir-bravin111 <mrbravin69@gmail.com>
2025-11-14 18:09:09 +0300

    Update tts.js

--- bravo/tts.js
+++ bravo/tts.js
@@ -6,11 +6,11 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
-const adams = require(__dirname + "/../config");
+const bravi = require(__dirname + "/../config");
 
 async function fetchTTSUrl() {
   try {
-    const response = await axios.get(adams.BWM_XMD);
+    const response = await axios.get(bravo.jeepers_creeper);
     const $ = cheerio.load(response.data);
 
     const targetElement = $('a:contains("TTS")');
